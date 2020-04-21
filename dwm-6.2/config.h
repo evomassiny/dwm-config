4756cfeb36192ff5de44d6d63ebf4c6f9f6c2dc6
evomassiny <hanzohatoryv@gmail.com>
2020-04-21 10:37:14 +0200

    Moved keys def in a seperate file

--- dwm-6.2/config.h
+++ dwm-6.2/config.h
@@ -1,58 +1,5 @@
 /* See LICENSE file for copyright and license details. */
-// other mapping
-#define CODE_return 36
-#define CODE_backspace 22
-#define CODE_tab 23
-#define CODE_space 65
-#define CODE_comma 58
-#define CODE_semicolon 59
-#define CODE_pageup 112
-#define CODE_pagedown 117
-#define CODE_screencap 107
-#define CODE_left_arrow 113
-#define CODE_right_arrow 114
-#define CODE_end 115
-
-// Main Numbers (not the actual numbers but the keys with them) 
-#define CODE_0 19
-#define CODE_1 10
-#define CODE_2 11
-#define CODE_3 12
-#define CODE_4 13
-#define CODE_5 14
-#define CODE_6 15
-#define CODE_7 16
-#define CODE_8 17
-#define CODE_9 18
-
-// letters mapping
-#define CODE_a 24
-#define CODE_z 25
-#define CODE_e 26
-#define CODE_r 27
-#define CODE_t 28
-#define CODE_u 30
-#define CODE_i 31
-#define CODE_o 32
-#define CODE_p 33
-
-#define CODE_q 38
-#define CODE_s 39
-#define CODE_d 40
-#define CODE_f 41
-#define CODE_g 42
-#define CODE_h 43
-#define CODE_j 44
-#define CODE_k 45
-#define CODE_l 46
-#define CODE_m 47
-
-#define CODE_w 52
-#define CODE_x 53
-#define CODE_c 54
-#define CODE_v 55
-#define CODE_b 56
-#define CODE_n 57
+#include "keys.h"
 
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
